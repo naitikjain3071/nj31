082eb72ebcfe44388a2d747c08430ba9e7379146
naitikjain3071 <70900997+naitikjain3071@users.noreply.github.com>
2021-04-08 16:17:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABSE_URI'] = 'mysql://priya:1234@localhost/xyz'
+app.config['SQLALCHEMY_DATABSE_URI'] = os.environ.get('DATABASE_URL')
+#app.config['SQLALCHEMY_DATABSE_URI'] = 'mysql://priya:1234@localhost/xyz'
+#app.config['DATABASE_URL'] = 'mysql://priya:1234@localhost/xyz'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 db = SQLAlchemy(app)
